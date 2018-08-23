48e53cd4743c590bec2f2162740c4a15cbd81eee
Ryan O'Beirne <robeirne@spoonflower.com>
2018-08-23 16:24:05 -0400

    Setup file validation and image scaling

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,51 @@ extern crate image;
 #[macro_use]
 extern crate clap;
 
+use image::GenericImage;
+
+fn validate_file(file: &str) -> bool {
+    if std::path::Path::new(file).is_file()
+    {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 fn main() {
-    let matches = clap_app!(prepress =>
+    // Parse command line arguments
+    let matches = clap_app!(stretchcomp =>
         (version: crate_version!())
         (author: crate_authors!())
         (about: crate_description!())
-        (@arg OUTPUTFILE: -o --output-file +takes_value "Specify output file name. May only be used with single file input")
+        (@arg widthscale: -w --width +takes_value +required "Specify width scaling percentage.")
+        (@arg heightscale: -h --height +takes_value +required "Specify height scaling percentage.")
         (@arg INPUTFILE: +required +multiple "Specify input file name(s).")
+        (@arg OUTPUTFILE: -o --outputfile +takes_value "Specify output file name. May only be used with single file input")
     ).get_matches();
 
     let input_files: Vec<&str> = matches.values_of("INPUTFILE").unwrap().collect();
+    let scale_w: f32 = matches.value_of("widthscale").unwrap().parse().unwrap();
+    let scale_h: f32 = matches.value_of("heightscale").unwrap().parse().unwrap();
 
+    // Validate that files exist
+    let mut valid_files: Vec<&str> = Vec::new();
     for file in input_files.iter() {
-        use std::path::Path;
-        let path_exists = Path::new(file).is_file();
-        println!("FILE: {}\t EXISTS: {:?}", file, path_exists);
+        if validate_file(file) {
+            valid_files.push(file);
+        }
+    }
+
+    for valid_file in valid_files.iter() {
+        let out: String = matches.value_of("OUTPUTFILE").unwrap_or(&format!("{}_pp.jpg", valid_file)).parse().unwrap();
+        let img = image::open(valid_file).unwrap();
+        let size_x = img.width();
+        let size_y = img.height();
+        let scaled_x = (size_x as f32 * scale_w / 100.0 ) as u32;
+        let scaled_y = (size_y as f32 * scale_h / 100.0 ) as u32;
+        println!("{}: {}% x {}%", valid_file, scale_w, scale_h);
+        println!("\t{} x {} => {} x {}", size_x, size_y, scaled_x, scaled_y);
+        img.resize_exact( scaled_x, scaled_y, image::FilterType::Nearest).save(&out).unwrap();
+        println!("\t=> {}", &out);
     }
 }
